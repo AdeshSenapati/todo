c52e3b2d538a91e49b830b05fdaea93a955b39b3
Adesh Senapati <91380484+AdeshSenapati@users.noreply.github.com>
2022-05-13 20:16:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 arr_list = [];
 temp = 0;
+let last = localStorage.length;
+if(last != 0) {
+    for (let j = 1; j <= last; j++) {
+        lm = localStorage.getItem(j)
+        console.log(lm);
+        add_prev(lm)
+    }
+}
+function add_prev(lm) {
+    temp = temp + 1;
+    let list_item = document.getElementById("list_item");
+    let make_li = document.createElement("DIV");
+    make_li.innerHTML = lm;
+    make_li.setAttribute("id", temp)
+    list_item.appendChild(make_li);   
+    make_li.onclick = function(){
+        arr_list.push(make_li.id);
+        make_li.style.backgroundColor = "#ffdeda";
+    }
+}
+console.log(last)
 function removeDuplicates(arr_list) {
     return arr_list.filter((item,
         index) => arr_list.indexOf(item) === index);
@@ -20,7 +41,7 @@ function add_item() {
         let cc = "NAME: "+item2.value+"<br>"+"DATE: "+item3.value+"<br>"+"TODO: "+item1.value;
         make_li.innerHTML = cc;
         make_li.setAttribute("id", temp)
-
+        localStorage.setItem(temp, cc);
         list_item.appendChild(make_li);
  
         item1.value=""
@@ -47,6 +68,7 @@ function delete_item(){
     console.log(arr)
     for(let i=0; i<arr.length; i++){
         document.getElementById(arr[i]).remove();
+        localStorage.removeItem(arr[i]);
     }
     arr_list = [];
 }
